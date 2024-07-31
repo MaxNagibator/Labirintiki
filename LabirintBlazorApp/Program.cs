53147e0f237be63e7ec083e088cf5be5918d0da3
onimor <onimor07@hotmail.com>
2024-07-31 14:44:23 +0300

    - Немного осовременили UI. - Добавили переключение между темной и светлой темой. - Переделали главную страницу (стала более адаптивной к размеру экрана и обобщенной). - На странице Labyrinth доработана ui составляющая параметров генерации. - Реализована фича с выбором цвета лабиринта (какой-то костыль вышел лютый из-за преколов с перерисовкой. Нужно будет зарефакторить как появится время)

--- LabirintBlazorApp/Program.cs
+++ LabirintBlazorApp/Program.cs
@@ -1,11 +1,14 @@
+using Blazored.LocalStorage;
 using LabirintBlazorApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
